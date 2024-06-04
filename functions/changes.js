a676a218f1cf1c1432437eb702f0d7cb0cd55037
Master5111 <pawel9395@wp.pl>
2024-06-04 22:28:12 +0200

    changes.js v0.5

--- functions/changes.js
+++ functions/changes.js
@@ -1,8 +1,7 @@
 import "dotenv/config"
-//import { parseFromString } from 'dom-parser'
 import {JSDOM} from "jsdom"
 
-async function logChanges() {
+async function decoder(/* response */) {
     /* 
     Changing ISO-8859-2 encoding into UTF-8
     */
@@ -12,14 +11,51 @@ async function logChanges() {
     response = new TextDecoder('iso-8859-2').decode(response)
     response = Buffer.from(response, 'utf8')
     response = response.toString('utf8')
-    const doc = new JSDOM(response)
+    
+    //console.log(response) Testing
+    //selectClass(doc)
     //console.log(response)
-    selectClass(doc)
+    return response
 }
 
-function selectClass(doc){
-   const len = doc.window.document.getElementsByClassName("st8").length
-   console.log(len)
+function selectClass(response){
+    const doc = new JSDOM(response)
+    const row = doc.window.document.getElementsByTagName("tr")
+    const len = row.length
+    const re = new RegExp("[2] [i,I,J]")
+
+    var count = 0
+    var obj = {changes:[], count: count, warnings: ""}
+
+    
+    for(var i = 0; i < len; i++){
+        if(!re.exec(row[i].innerHTML)){continue} //Exec only on matching rows
+        //console.log(row[i].getElementsByTagName("td")[0].innerHTML)
+        var split = row[i].getElementsByTagName("td")[1].innerHTML.split(" - ")
+
+
+        obj.changes.push({
+            hour: Number(row[i].getElementsByTagName("td")[0].innerHTML.trim()),
+            class: split[0].trim().replace("&nbsp;",""),
+            //fromTeacher:
+            toTeacher: row[i].getElementsByTagName("td")[2].innerHTML.trim().replace("&nbsp;",""),
+            change: split[1].trim().replace("&nbsp;",""),
+            warnings: row[i].getElementsByTagName("td")[3].innerHTML.trim().replace("&nbsp;","")
+        })
+        count++        
+    }
+
+
+    obj.changes.sort(function(obj1, obj2) {
+        // Ascending: first hour less than the previous
+        return obj1.hour - obj2.hour
+    })
+    obj.count = count
+    obj.warnings = doc.window.document.getElementsByTagName("nobr")[0].innerHTML.trim()
+    console.log(obj)
 }
 
-logChanges()
+
+decoder().then(x => {
+    selectClass(x)
+})
