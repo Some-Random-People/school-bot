f50d41bbe86c5363066a9cbaccbcee1382bd4b46
rysiek21 <szuda.michal@gmail.com>
2024-09-14 22:53:04 +0200

    Translation handling

--- functions/lang-handler.js
+++ functions/lang-handler.js
@@ -1,5 +1,20 @@
 import 'dotenv/config'
+import * as fs from 'fs'
 
-function Translate(string, ...args) {
-    
+export function Translate(translationString, ...args) {
+    try {
+        let data = fs.readFileSync(`./locales/${process.env.LOCALES}.json`, 'utf8')
+        data = JSON.parse(data) 
+        if(!data[translationString])
+            console.error(`Can't find translation`)
+        let translation = data[translationString]
+        if(args.length > 0) {
+            args.forEach(element => {
+                translation = translation.replace('%s', element)
+            })
+        }
+        return translation
+    } catch (err) {
+        console.error(err)
+    }
 }
