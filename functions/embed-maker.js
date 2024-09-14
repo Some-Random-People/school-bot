cbbf2f00154f8ae563237193ad06c8ae545ca541
rysiek21 <szuda.michal@gmail.com>
2024-09-14 22:00:04 +0200

    webhook rewrite and lang files

--- functions/embed-maker.js
+++ functions/embed-maker.js
@@ -1,15 +1,34 @@
-import { EmbedBuilder } from "discord.js"
+import { EmbedBuilder, WebhookClient } from "discord.js"
+import 'dotenv/config'
 
-export function makeEmbed(data) {
+export function makeMessage(data) {
+    let fields = []
+    data.forEach(element => {
+        fields.push({})
+    })
     const embed = new EmbedBuilder()
         .setColor(0xAA11BF)
-        .setTitle('TEST')
+        .setTitle('Zastępstwa')
         .setURL(process.env.WEBSITE)
-        .setDescription('TEST')
+        .setDescription('Ilość zastępstw: 2')
         .addFields(
-            { name: 'TEST', value: 'TEST' },
+            { name: 'Informacje', value: 'Zastępstwa w dniu 05.06.2024 środa \nDrugie?' },
+            { name: 'Godzina: 1', value: 'Klasa: 4 I Zmiana: matematyka - 205 Nauczyciel: Torzilla'},
+            { name: 'Godzina: 2', value: 'Klasa: 4 I Zmiana: matematyka - 205 Nauczyciel: Torzilla'},
+            { name: 'Godzina: 2', value: 'Klasa: 4 I Zmiana: matematyka - 205 Nauczyciel: Torzilla'},
+            { name: 'Godzina: 2', value: 'Klasa: 4 I Zmiana: matematyka - 205 Nauczyciel: Torzilla'},
+            { name: 'Godzina: 2', value: 'Klasa: 4 I Zmiana: matematyka - 205 Nauczyciel: Torzilla'},
+            { name: 'Godzina: 2', value: 'Klasa: 4 I Zmiana: matematyka - 205 Nauczyciel: Torzilla'},
+            { name: 'Godzina: 2', value: 'Klasa: 4 I Zmiana: matematyka - 205 Nauczyciel: Torzilla'},
+            { name: 'Godzina: 2', value: 'Klasa: 4 I Zmiana: matematyka - 205 Nauczyciel: Torzilla'},
+            { name: 'Godzina: 2', value: 'Klasa: 4 I Zmiana: matematyka - 205 Nauczyciel: Torzilla'},
         )
         .setTimestamp()
-        .setFooter({ text: 'TEST', iconURL: 'https://avatars.githubusercontent.com/u/123210096?s=1000&v=4' })
-    return embed
+        .setFooter({ text: 'Some Random People', iconURL: 'https://avatars.githubusercontent.com/u/123210096?s=1000&v=4' })
+    const webhookClient = new WebhookClient({ url: process.env.WEBHOOK })
+    webhookClient.send({
+        content: 'Webhook',
+        username: 'Szkola',
+        embeds: [embed],
+    })
 }
--- index.js
+++ index.js
@@ -1,35 +1,3 @@
-import { Client, Collection, GatewayIntentBits } from 'discord.js'
 import 'dotenv/config'
-import * as data from './commands/changes.js'
-import { check } from './functions/check-handler.js'
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] })
-
-client.commands = new Collection();
-client.commands.set('changes', data);
-
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`)
-    check(client.channels.cache.get(process.env.CHANNEL_ID))
-})
-
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-	const command = interaction.client.commands.get(interaction.commandName);
-	if (!command) {
-		console.error(`No command matching ${interaction.commandName} was found.`);
-		return;
-	}
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		if (interaction.replied || interaction.deferred) {
-			await interaction.followUp({ content: 'There was an error while executing this command!', ephemeral: true });
-		} else {
-			await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-		}
-	}
-});
-
-client.login(process.env.TOKEN)
+print('Running school')
