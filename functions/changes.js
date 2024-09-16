1a37c5580b2aab3026323c6c17ded1977606d72e
rysiek21 <szuda.michal@gmail.com>
2024-09-16 17:10:35 +0200

    Running program in index

--- functions/changes.js
+++ functions/changes.js
@@ -12,10 +12,6 @@ async function decoder() {
     response = new TextDecoder('iso-8859-2').decode(response)
     response = Buffer.from(response, 'utf8')
     response = response.toString('utf8')
-    
-    //console.log(response) Testing
-    //selectClass(doc)
-    //console.log(response)
     return response
 }
 
@@ -23,7 +19,6 @@ function selectClass(response){
     const doc = new JSDOM(response)
     const row = doc.window.document.getElementsByTagName("tr")
     const len = row.length
-    console.log(process.env.CLASS)
     const class_split = process.env.CLASS.split(" ")
     const re = new RegExp(`[${class_split[0]}] [${class_split[1]}]`)
 
@@ -32,13 +27,11 @@ function selectClass(response){
 
     for(var i = 0; i < len; i++){
         if(!re.exec(row[i].innerHTML)){continue} //Exec only on matching rows
-        //console.log(row[i].getElementsByTagName("td")[0].innerHTML)
         var split = row[i].getElementsByTagName("td")[1].innerHTML.split(" - ")
 
         obj.changes.push({
             hour: Number(row[i].getElementsByTagName("td")[0].innerHTML.trim()),
             class: split[0].trim().replace("&nbsp;",""),
-            //fromTeacher:
             toTeacher: row[i].getElementsByTagName("td")[2].innerHTML.trim().replace("&nbsp;",""),
             change: split[1].trim().replace("&nbsp;",""),
             warnings: row[i].getElementsByTagName("td")[3].innerHTML.trim().replace("&nbsp;","")
@@ -55,9 +48,8 @@ function selectClass(response){
     makeMessage(obj)
 }
 
-function changes(){
+export function changes(){
     return decoder().then(x => {
         return selectClass(x)
     })
 }
-//changes()
--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+import * as crypto from 'crypto'
+import { changes } from './functions/changes.js'
 import 'dotenv/config'
 
-print('Running school')
+async function check() {
+    let last = ''
+    while(true) {
+        await new Promise(resolve => { setTimeout(resolve, 60000) })
+        let hash = ''
+        let response = await fetch(process.env.WEBSITE)
+        response.text().then(text => hash = crypto.createHash('sha256').update(text).digest('hex'))
+        if (last == '') { 
+            last = hash
+        }
+        if(last == hash)
+            continue
+        changes()
+    }
+}
+
+check()
